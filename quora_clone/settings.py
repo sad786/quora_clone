682b0d125bb3cec70ce388fe9e9106dc6e635a85
Saddam Ahmed <saddam.groot@gmail.com>
2025-04-08 08:16:40 +0530

    Update settings.py

--- quora_clone/settings.py
+++ quora_clone/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.middleware.WhitenoiseMiddleware'
+    'whitenoise.milddeware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
